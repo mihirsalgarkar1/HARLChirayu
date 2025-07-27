206c038e2b08a6a684895da5445df3f275d97992
Mihir Salgarkar <mihir.salgarkar@ingeniousmed.com>
2025-07-27 13:35:46 -0400

    WIP: YAML changes, Various __init__.py changes

--- harl/algorithms/actors/__init__.py
+++ harl/algorithms/actors/__init__.py
@@ -1,6 +1,9 @@
 """Algorithm registry."""
 from harl.algorithms.actors.happo import HAPPO
 from harl.algorithms.actors.hatrpo import HATRPO
+from harl.algorithms.actors.adaptive_hatrpo import Adaptive_HATRPO  #WIP - Added; Please check this
+#from harl.algorithms.actors.adaptive_wasserstein import ?          #WIP - To be Added; Nothing is defined there for now; Please check this
+#from harl.algorithms.actors.nonadaptive_wasserstein import ?       #WIP - To be Added; Nothing is defined there for now; Please check this
 from harl.algorithms.actors.haa2c import HAA2C
 from harl.algorithms.actors.haddpg import HADDPG
 from harl.algorithms.actors.hatd3 import HATD3
@@ -13,6 +16,9 @@ from harl.algorithms.actors.mappo import MAPPO
 ALGO_REGISTRY = {
     "happo": HAPPO,
     "hatrpo": HATRPO,
+    "adaptive_hatrpo": Adaptive_HATRPO,         #WIP - Added; Please check this
+#    "adaptive_wasserstein": ?,                 #WIP - To be Added; Nothing is defined there for now; Please check this
+#    "nonadaptive_wasserstein": ?,              #WIP - To be Added; Nothing is defined there for now; Please check this
     "haa2c": HAA2C,
     "haddpg": HADDPG,
     "hatd3": HATD3,
